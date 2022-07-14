e399b90f93338cddc913aee94f7e59462e2f1fe3
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-14 12:17:20 +0800

    Added: custom fonts

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { Button } from "react-bootstrap";
 
 export default function Home() {
   return (
     <>
       <h1>HELLO WORLD!</h1>
+      <Button>TESTINGGGGG</Button>
     </>
   );
 }

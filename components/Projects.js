04cf7c477604a6f344b9853eaf744c40aa270584
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-15 14:24:48 +0800

    Added: projects component

---
+++ components/Projects.js
@@ -0,0 +1,20 @@
+import styles from "../styles/COMPONENT.module.scss";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Projects = () => {
+  return (
+    <>
+      <div className={styles._main_component_wrapper}>
+        <Container fluid="lg" className="p-0">
+          <Row className="gx-3 gy-4">
+            <Col>
+                
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Navigation from "../components/Nav";
 import Introduction from "../components/Introduction";
+import Projects from "../components/Projects";
 
 export default function Home() {
   return (
     <>
       <Navigation />
       <Introduction />
+      <Projects />
     </>
   );
 }

db564155b86291978f5711ed8bae7d5873ef84e3
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-15 19:48:28 +0800

    Added: about component

---
+++ components/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <>
+      Officia reprehenderit consequat incididunt occaecat ullamco magna irure
+      aute proident et nulla. Non labore proident pariatur tempor nostrud
+      cupidatat dolor. Lorem fugiat reprehenderit do dolore laboris est veniam
+      ea adipisicing aliqua incididunt cupidatat. Anim dolore consectetur nisi
+      ut pariatur officia quis sit elit elit pariatur aute aliqua. Ea pariatur
+      id officia eiusmod commodo voluptate nulla magna cupidatat velit occaecat
+      culpa. In tempor fugiat ut laborum ea duis sunt adipisicing labore. Minim
+      ex proident id magna eu minim.
+    </>
+  );
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Navigation from "../components/Nav";
 import Introduction from "../components/Introduction";
 import Projects from "../components/Projects";
+import About from "../components/About";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navigation />
       <Introduction />
       <Projects />
+      <About />
     </>
   );
 }

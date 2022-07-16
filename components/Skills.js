661d543e40a70a589222900d12f4f7b10b248c6a
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-16 15:14:13 +0800

    Added: skills component

---
+++ components/Skills.js
@@ -0,0 +1,14 @@
+const Skills = () => {
+  return (
+    <>
+      Excepteur officia nulla qui amet est commodo labore. Cupidatat laborum
+      quis aliquip ad. In qui enim anim excepteur occaecat laboris. Duis sunt
+      eiusmod ullamco est id. Nisi pariatur nulla nulla magna occaecat
+      exercitation do aliquip duis aliquip fugiat aliqua. Officia id veniam
+      mollit et deserunt velit ullamco proident sint ad excepteur pariatur
+      irure.
+    </>
+  );
+};
+
+export default Skills;

c87ade95b8a13feeb285bbc505b5e703db87f2b4
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-16 15:15:34 +0800

    Fixed: issues at about component

--- components/About.js
+++ components/About.js
@@ -9,11 +9,11 @@ const About = () => {
           <h3 className={styles._h3_title}>About</h3>
 
           <p className={styles._about_paragraph_}>
-            Hi, my name is JK Lagrosa, and I'm a Front-end web developer. I
+            Hi, my name is JK Lagrosa, and I&apos;m a Front-end web developer. I
             designed and developed websites and web applications with a focus on
             usability, accessibility, and performance. Using technologies such
             as HTML/CSS, JavaScript, React, Next.js, MongoDB, Bootstrap, and
-            Sass. I'm passionate about building responsive, dynamic,
+            Sass. I&apos;m passionate about building responsive, dynamic,
             SEO-friendly websites and web apps.
           </p>
         </Container>

a4545b21c149bf7a315e3dad7161c4c9f5b60358
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-16 14:52:01 +0800

    Added: about component styling

--- components/About.js
+++ components/About.js
@@ -1,14 +1,23 @@
+import styles from "../styles/COMPONENT.module.scss";
+import { Container } from "react-bootstrap";
+
 const About = () => {
   return (
     <>
-      Officia reprehenderit consequat incididunt occaecat ullamco magna irure
-      aute proident et nulla. Non labore proident pariatur tempor nostrud
-      cupidatat dolor. Lorem fugiat reprehenderit do dolore laboris est veniam
-      ea adipisicing aliqua incididunt cupidatat. Anim dolore consectetur nisi
-      ut pariatur officia quis sit elit elit pariatur aute aliqua. Ea pariatur
-      id officia eiusmod commodo voluptate nulla magna cupidatat velit occaecat
-      culpa. In tempor fugiat ut laborum ea duis sunt adipisicing labore. Minim
-      ex proident id magna eu minim.
+      <div className={styles._about_component_wrapper}>
+        <Container fluid="lg" className="p-0">
+          <h3 className={styles._h3_title}>About</h3>
+
+          <p className={styles._about_paragraph_}>
+            Hi, my name is JK Lagrosa, and I'm a Front-end web developer. I
+            designed and developed websites and web applications with a focus on
+            usability, accessibility, and performance. Using technologies such
+            as HTML/CSS, JavaScript, React, Next.js, MongoDB, Bootstrap, and
+            Sass. I'm passionate about building responsive, dynamic,
+            SEO-friendly websites and web apps.
+          </p>
+        </Container>
+      </div>
     </>
   );
 };

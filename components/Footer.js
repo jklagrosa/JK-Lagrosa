8e2d16c94b42856c5a225781ad5a7cc89456f1e7
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-17 18:47:25 +0800

    Added: media queries to footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,136 @@
+import styles from "../styles/COMPONENT.module.scss";
+import { Container, Row, Col } from "react-bootstrap";
+
 const Footer = () => {
   return (
     <>
-      Duis culpa eiusmod aute occaecat id. Fugiat ipsum quis esse exercitation
-      occaecat eu elit nulla. Minim sint adipisicing exercitation excepteur
-      voluptate ad pariatur est nisi nulla nostrud ex ad proident. Magna qui
-      minim quis ad. Eu ut aute nostrud aliquip labore.
+      <div className={styles._footer_component_wrapper}>
+        <Container fluid="lg" className="p-0">
+          <Row className="gx-3 gy-5 mx-auto" id={styles._footer_row_wrapper_}>
+            <Col xs={12} md={6} lg={6} id={styles._footer_cols_wrapper_}>
+              <h1 className={styles._footer_logo_icon_}>JKL</h1>
+              <p className={styles._footer_desc_}>
+                I designed and developed websites and web applications with a
+                focus on usability, accessibility, and performance. Using
+                technologies such as HTML/CSS, JavaScript, React, Next.js,
+                MongoDB, Bootstrap, and Sass.
+              </p>
+            </Col>
+            {/* ### */}
+            <Col xs={12} md={6} lg={3} id={styles._footer_cols_wrapper_}>
+              <h6 className={styles._footer_h6_header_}>Contact me</h6>
+              <div className={styles._footer_links_tag_wrapper_}>
+                <a
+                  href="https://www.linkedin.com/in/jklagrosa/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  LinkedIn
+                </a>
+                <br />
+                <a
+                  href="https://github.com/jklagrosa"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  Github
+                </a>
+                <br />
+                <a
+                  href="mailto:jklagrosa.dev@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  Gmail
+                </a>
+                <br />
+              </div>
+            </Col>
+            {/* ### */}
+            <Col xs={12} md={6} lg={3} id={styles._footer_cols_wrapper_}>
+              <h6 className={styles._footer_h6_header_}>Tech Used</h6>
+              <div className={styles._footer_links_tag_wrapper_}>
+                <a
+                  href="https://html.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  HTML
+                </a>
+                <br />
+                <a
+                  href="https://www.w3.org/Style/CSS/Overview.en.html"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  CSS
+                </a>
+                <br />
+                <a
+                  href="https://sass-lang.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  Sass
+                </a>
+                <br />
+                <a
+                  href="https://getbootstrap.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  Bootstrap
+                </a>
+                <br />
+                <a
+                  href="https://www.javascript.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  JavaScript
+                </a>
+                <br />
+                <a
+                  href="https://reactjs.org/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  React
+                </a>
+                <br />
+                <a
+                  href="https://nextjs.org/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  Next.js
+                </a>
+                <br />
+                <a
+                  href="https://www.mongodb.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles._footer_links_tag_}
+                >
+                  MongoDB
+                </a>
+                <br />
+              </div>
+            </Col>
+            {/* ### */}
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Projects from "../components/Projects";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <About />
       <Skills />
       <Contact />
+      <Footer />
     </>
   );
 }

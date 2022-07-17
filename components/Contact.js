6914b7b7586ef91704d7ba2fe92d6438c11959dc
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-17 14:26:16 +0800

    Added: contact links tag

--- components/Contact.js
+++ components/Contact.js
@@ -9,13 +9,51 @@ const Contact = () => {
           <h3 className={styles._h3_title}>Contact</h3>
 
           <Row className="gx-3 gy-4 mx-auto">
-            <Col xs={12} lg={8} className="order-2 order-lg-1">
+            <Col xs={12} lg={6} className="order-2 order-lg-1">
               <h5 className={styles._contact_h5_tag_}>Let&apos;s talk</h5>
-              <p className={styles._contact_paragraph_}></p>
+              <p className={styles._contact_paragraph_}>
+                Do you have a question or proposal? My linkedin, github, and
+                gmail accounts are also accessible.
+              </p>
+
+              <a className={styles._contact_links_tag_}>LinkedIn</a>
+              <a className={styles._contact_links_tag_}>Github</a>
+              <a className={styles._contact_links_tag_}>Gmail</a>
             </Col>
             {/* ### */}
-            <Col xs={12} lg={4} className="order-1 order-lg-2">
-              <form></form>
+            <Col xs={12} lg={6} className="order-1 order-lg-2">
+              <form>
+                <label>Name</label>
+                <input
+                  type="text"
+                  required
+                  minLength={2}
+                  placeholder="e.g. JK Lagrosa"
+                />
+                {/* ### */}
+                <label>Email</label>
+                <input
+                  type="email"
+                  required
+                  minLength={2}
+                  placeholder="e.g. example@gmail.com"
+                />
+                {/* ### */}
+                <label>Message</label>
+                <textarea
+                  rows="10"
+                  cols="30"
+                  required
+                  minLength={2}
+                  placeholder="Your message"
+                ></textarea>
+                {/* ### */}
+                <button type="submit">Send message</button>
+
+                <p className={styles._form_message_error_}>
+                  *All fields required.
+                </p>
+              </form>
             </Col>
           </Row>
         </Container>

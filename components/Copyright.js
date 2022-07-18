a91eb3691afcb45ea0e1f5853f5f5e5e8dc6d680
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-18 13:34:28 +0800

    Added: media query to copyright component

--- components/Copyright.js
+++ components/Copyright.js
@@ -1,10 +1,17 @@
+import styles from "../styles/COMPONENT.module.scss";
+import { Container } from "react-bootstrap";
+
 const Copyright = () => {
   return (
     <>
-      Deserunt deserunt proident elit nulla amet. Nulla reprehenderit qui velit
-      est pariatur mollit irure aute. Commodo labore in ad ea commodo culpa
-      proident eiusmod adipisicing est ipsum id deserunt culpa. Ipsum duis amet
-      nisi commodo duis et laborum cupidatat do laborum.
+      <div className={styles._copyright_component_wrapper}>
+        <Container fluid="lg" className="p-0">
+          <p>
+            Copyright &copy; {new Date().getFullYear()}, JK Lagrosa. All Rights
+            Reserved.
+          </p>
+        </Container>
+      </div>
     </>
   );
 };

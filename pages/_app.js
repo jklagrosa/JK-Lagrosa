8b5318425cd696883663173386e455e09db64e3a
jklagrosa <jklagrosa.dev@gmail.com>
2022-07-19 16:02:05 +0800

    Added: title and meta in head for SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import "../styles/globals.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>JK Lagrosa • Front-End Web Developer, Philippines</title>
+        <meta
+          name="description"
+          content="Hi, my name is JK Lagrosa, and I'm a Front-end web developer from the Philippines. I designed and developed websites and web applications with a focus on usability, accessibility, and performance. Using technologies such as HTML, CSS, JavaScript, React, Next.js, MongoDB, Bootstrap, and Sass. I'm passionate about building responsive, dynamic, SEO-friendly websites and web apps."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
